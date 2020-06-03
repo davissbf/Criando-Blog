5dfa3440b35c60fe80ff8349a1aeea7d87e771ff
Davi Souza <davissbf@gmail.com>
2020-06-03 20:18:32 -0300

    criação de artigos

--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 
 const connection = new Sequelize('blog', 'root', 'NODE2020express', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: "-03:00"
 });
 
 module.exports = connection;
