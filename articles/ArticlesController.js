1401ac131bb9198ab6a667244b58d9d94db84e88
Davi Souza <davissbf@gmail.com>
2020-06-06 02:26:52 -0300

    fim artigos e categorias

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,7 +5,11 @@ const Article = require("./Article");
 const slugify = require("slugify");
 
 router.get("/admin/articles", (req, res)=>{
-  res.send("Rota de artigos");
+  Article.findAll({
+    include:[{model: Category}]
+  }).then(articles =>{
+    res.render("admin/articles/index", {articles: articles});
+  });
 });
 
 router.get("/admin/articles/new", (req, res)=>{
@@ -26,7 +30,29 @@ router.post("/articles/save", (req, res)=> {
     categoryId: category
   }).then(() => {
     res.redirect("/admin/articles");
-  });
+  }).catch(err => console.log(err));
+});
+
+
+router.post("/articles/delete", (req, res)=>{
+  let id = req.body.id;
+
+  if(id !== undefined){
+    if(!isNaN(id)){
+      Article.destroy({
+        where: {
+          id: id
+        }
+      }).then(()=>{
+        res.redirect("/admin/articles")
+      });
+
+    }else{ // se o id não for um numero
+      res.redirect("/admin/articles");
+    }
+  }else{ // NULL
+    res.regirect("/admin/articles");
+  }
 });
 
 module.exports = router;
